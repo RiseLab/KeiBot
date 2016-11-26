f92ffbf9ef4e60bd753f5fd1f94e33098216f5e0
Kei <riselabhq@gmail.com>
2016-11-26 20:02:59 +0000

    small changes for test git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    KeiBot
                 </div>
 
                 <div class="links">
